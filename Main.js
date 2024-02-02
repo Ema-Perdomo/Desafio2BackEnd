e4d5b74b44e70a8197e5ee45d4f10e90910bc354
EmanuelPerdomo <emaperdomo00@gmail.com>
2024-02-01 22:44:20 -0300

    Correccion preentrega

--- Main.js
+++ Main.js
@@ -1,20 +1,22 @@
 import { Product } from "./Product.js";
 import { ProductManager } from "./ProductManager.js";
 
-// const producto1 = new Product("Arroz","Muy Rico", 40, 100, "A123" )
-// const producto2 = new Product("Lentejas","Muy sanas", 60, 100, "B123" )
-// const producto3 = new Product("Yerba Mate","Amarga", 180, 100, "C123" )
-// const producto4 = new Product("Azucar","Azucarlito", 56, 100, "D123" )
+const producto1 = new Product("Arroz","Muy Rico", 40, 100, "A123","img" )
+const producto2 = new Product("Lentejas","Muy sanas", 60, 100, "B123","img" )
+const producto3 = new Product("Yerba Mate","Amarga", 180, 100, "C123","img" )
+const producto4 = new Product("Azucar","Azucarlito", 56, 100, "D123","img" )
 
 //Tests
 const productManager = new ProductManager ('./products.json')
-console.log(productManager.getProducts())
-productManager.addProduct( 'producto prueba','Este es un producto prueba', 200,[],'abc123',25)
-console.log(productManager.getProducts())
-console.log(productManager.getProductsById('abc123'))
-const productoCambio = new Product("Azucar","Azucarlito", 56, 100, "D123" )
-console.log(productManager.updateProduct('abc123',productoCambio))
-console.log(productManager.deleteProduct('abc123'))
+// productManager.getProducts()
+
+
+ //productManager.addProduct( producto1)
+// productManager.getProducts()
+//productManager.getProductsById('abc123')
+//productManager.getProductsById('c1bb105e3a1e723ceb51')
+//productManager.updateProduct('c1bb105e3a1e723ceb51',producto2)
+productManager.deleteProduct('c1bb105e3a1e723ceb51')
 
 
 
--- ProductManager.js
+++ ProductManager.js
@@ -4,76 +4,76 @@ export class ProductManager {
     constructor(path) {
         this.path = path
     }
-}
 
-//Añado un producto
-async addProduct(producto) {
-    const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
-    if (producto.title && producto.description &&
-        producto.price && producto.thumbnail && producto.code &&
-        producto.stock) {
-        const code = products.findIndex((p) => p.code === producto.code)
-        if (code === -1) {
-            products.push(producto)
-            await fs.writeFile(this.path, JSON.stringify(products))
-            console.log('Producto creado correctamente')
+    //Añado un producto
+    async addProduct(producto) {
+        const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+        if (producto.title && producto.description &&
+            producto.price && producto.thumbnail && producto.code &&
+            producto.stock) {
+            const code = products.findIndex((p) => p.code === producto.code)
+            if (code === -1) {
+                products.push(producto)
+                await fs.writeFile(this.path, JSON.stringify(products))
+                console.log('Producto creado correctamente')
+            } else {
+                console.log("Este producto ya existe.")
+            }
         } else {
-            console.log("Este producto ya existe.")
+            console.log("Complete todos los campos!")
+
         }
-    } else {
-        console.log("Complete todos los campos!")
-
-    }
-}
-
-//Traigo productos
-async getProducts() {
-    const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
-    console.log(products)
-}
-
-//Traigo un producto segun su id
-async getProductsById(id) {
-    const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
-    const prod = products.find((p) => p.id == id)
-    if (prod) {
-        console.log(prod)
-    } else {
-        console.log("404: Not Found")
     }
 
-}
-//Actualizo un producto
-async updateProduct(id, nuevoProducto) {
-    const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
-    const indice = products.findIndex((p) => p.id == id)
-    if (indice != -1) {
-        products[indice].stock = nuevoProducto.stock;
-        products[indice].price = nuevoProducto.price;
-        products[indice].title = nuevoProducto.title;
-        products[indice].thumbnail = nuevoProducto.thumbnail;
-        products[indice].description = nuevoProducto.description;
-        products[indice].code = nuevoProducto.code;
-        await fs.writeFile(this.path, JSON.stringify(products))
-        console.log('Producto actualizado')
-
-    } else {
-        console.log("404: Not Found")
+    //Traigo productos
+    async getProducts() {
+        const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+        console.log(products)
     }
-}
-//Elimino producto dado su Id
+
+    //Traigo un producto segun su id
+    async getProductsById(id) {
+        const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+        const prod = products.find((p) => p.id == id)
+        if (prod) {
+            console.log(prod)
+        } else {
+            console.log("404: Not Found")
+        }
+
+    }
+    //Actualizo un producto
+    async updateProduct(id, nuevoProducto) {
+        const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+        const indice = products.findIndex((p) => p.id == id)
+        if (indice != -1) {
+            products[indice].stock = nuevoProducto.stock;
+            products[indice].price = nuevoProducto.price;
+            products[indice].title = nuevoProducto.title;
+            products[indice].thumbnail = nuevoProducto.thumbnail;
+            products[indice].description = nuevoProducto.description;
+            products[indice].code = nuevoProducto.code;
+            await fs.writeFile(this.path, JSON.stringify(products))
+            console.log('Producto actualizado')
+        } else {
+            console.log("404: Not Found")
+        }
+    }
+    //Elimino producto dado su Id
     async deleteProduct(id) {
         const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
         const indice = products.findIndex((p) => p.id === id);
         if (indice != -1) {
             const productosFiltrados = products.filter((p) => p.id != id);
-            await fs.writeFile(this.path, JSON.stringify(products))
+            await fs.writeFile(this.path, JSON.stringify(productosFiltrados))
             console.log('Producto eliminado')
         } else {
             console.log("404: Not Found")
         }
     }
 }
+
+
 
 // class ProductManager {
 //     constructor() {
